0e9878e0ff51fdcd6ddde926b633b70c3c7a1fca
Bruno Fernando <bruno.fernando81@hotmail.com>
2023-08-22 14:11:06 -0300

    Start the main project

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+
+const App = () => {
+
+    return (
+      <>
+       <div className="bg-gradient-to-l from-sky-600 to-blue-600 w-screen h-screen text-black flex justify-center items-center">
+          <section className="w-1/3 h-80 rounded-md p-6 bg-slate-200">
+            <section className="flex justify-center align-center">
+                <div>
+                  <h4>Quiz de Culinária</h4>
+                  <hr/>
+                  <h3>1. Quais ingredientes não podem faltar</h3>
+                </div>
+            </section>
+          </section>
+       </div>
+      </>
+    
+  )   
+}
+ 
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const App = () => {
+//     return (
+//        <div className={styled.base}>
+//             <div className={styled.top}>Top</div>
+//             <div className={styled.flex}>
+//                 <Left />
+//                 <Base />
+//                 <Right />
+//             </div>
+//             <div className={styled.bottom}>Bottom</div>
+//        </div>
+//     )   
+// }
+
+
+
