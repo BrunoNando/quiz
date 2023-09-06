3143c945f36c558cee1ae39951031a941e7ed9a3
Bruno Fernando <bruno.fernando81@hotmail.com>
2023-09-06 20:54:28 -0300

    Inicialization in project and craete the header

---
+++ README.md
@@ -0,0 +1,5 @@
+Projeto de um quiz
+
+Criado por Bruno Fernando através do curso 
+
+[Projeto B7Web](https://b7web.com.br/fullstack/?gclid=EAIaIQobChMIpZSk34z5gAMVsUFIAB111AS7EAAYASAAEgJpu_D_BwE)
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
+import { useState } from "react";
+import { questions } from "./data/questions";
 
 const App = () => {
+  const title = "Quiz de Culinária";
+  const [currentQuestion, setCurrentQuestion] = useState(0)
 
     return (
-      <>
        <div className="bg-gradient-to-l from-sky-600 to-blue-600 w-screen h-screen text-black flex justify-center items-center">
-          <section className="w-1/3 h-80 rounded-md p-6 bg-slate-200">
-            <section className="flex justify-center align-center">
-                <div>
-                  <h4>Quiz de Culinária</h4>
-                  <hr/>
-                  <h3>1. Quais ingredientes não podem faltar</h3>
-                </div>
-            </section>
+          <section className="w-1/3 h-80 rounded-md p-6 bg-slate-200 shadow shadow-black">
+            <div className="p-5 font-bold text-2xl rounded-md border-b border-gray-300">{title}</div>
+              <div className="p-5">
+                ...
+              </div>
+              <div className="p-5 text-center border-t border-gray-300">
+                {currentQuestion + 1} de {questions.length} pergunta{questions.length === 1 ? '' : 's'}
+              </div>
           </section>
-       </div>
-      </>
-    
+       </div>    
   )   
 }
  
